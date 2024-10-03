f90742bcba8c27c39792ce90965d8cd4d7f28c74
Mikael Carlsson <mickec79@gmail.com>
2024-10-03 14:50:29 +0200

    extracts redis-store from plugin

--- plugins/redis.ts
+++ plugins/redis.ts
@@ -1,30 +1,7 @@
 import fastifyPlugin from 'fastify-plugin'
 import fastifyRedis from '@fastify/redis'
 import { FastifyPluginCallback } from 'fastify'
-import { Redis, Callback } from 'ioredis'
-
-export const redisClient = new Redis({ host: 'localhost', port: 6379 })
-
-export const redisStore = {
-  get: async (sessionId: string, callback: Callback) => {
-    await redisClient.get(`sess:${sessionId}`, (err, result) => {
-      if (err) return callback(err)
-      callback(null, result ? JSON.parse(result) : null)
-    })
-  },
-  set: async (sessionId: string, session: unknown, callback: Callback) => {
-    await redisClient.set(
-      `sess:${sessionId}`,
-      JSON.stringify(session),
-      'EX',
-      86400,
-      callback
-    )
-  },
-  destroy: async (sessionId: string, callback: Callback) => {
-    await redisClient.del(`sess:${sessionId}`, callback)
-  },
-}
+import { redisClient } from '../utils/redis-store'
 
 const pluginCallback: FastifyPluginCallback = (fastify, opts, done) => {
   fastify.register(fastifyRedis, {
