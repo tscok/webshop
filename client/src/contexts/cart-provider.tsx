4e5332241927a3de35666d9d3d06c3fa1f82e76b
Mikael Carlsson <mickec79@gmail.com>
2024-10-10 12:09:02 +0200

    implements react queary

--- client/src/contexts/cart-provider.tsx
+++ client/src/contexts/cart-provider.tsx
@@ -1,37 +1,52 @@
-import { createContext, PropsWithChildren, useCallback } from 'react'
-import { Cart, ProductName } from '../../../types'
-import cartClient from '../api/cart-client'
-import useQuery from '../hooks/use-query'
+import { createContext, PropsWithChildren } from 'react'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { Cart, Product, ProductName } from '../../../types'
+import Api from '../api'
+import { queryClient } from '../state/query'
+
+const api = new Api()
 
 type CartType = {
   cart: Cart
-  onAdd: (name: ProductName) => Promise<void>
-  onRemove: (name: ProductName) => Promise<void>
+  onAdd: (name: ProductName) => void
+  onRemove: (name: ProductName) => void
+  products: Product[]
 }
 
-const DEFAULT_DATA: Cart = { count: 0, items: [], total: 0 }
+const DEFAULT_CART: Cart = { count: 0, items: [], total: 0 }
 
 export const CartContext = createContext<CartType>({} as CartType)
 
 export const CartProvider = ({ children }: PropsWithChildren) => {
-  const { data: cart = DEFAULT_DATA, refetch } = useQuery(
-    useCallback(async () => await cartClient.getCart(), [])
-  )
+  const { data: cart = DEFAULT_CART } = useQuery({
+    queryKey: ['cart'],
+    queryFn: api.getCart,
+  })
 
-  const handleAdd = async (name: ProductName) => {
-    await cartClient.addItem(name)
-    refetch()
-  }
+  const { mutate: onAdd } = useMutation({
+    mutationFn: api.addCartItem,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['cart'] })
+    },
+  })
 
-  const handleRemove = async (name: ProductName) => {
-    await cartClient.removeItem(name)
-    refetch()
-  }
+  const { mutate: onRemove } = useMutation({
+    mutationFn: api.delCartItem,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['cart'] })
+    },
+  })
+
+  const { data: products = [] } = useQuery({
+    queryKey: ['products'],
+    queryFn: api.getProducts,
+  })
 
   const value: CartType = {
     cart,
-    onAdd: handleAdd,
-    onRemove: handleRemove,
+    onAdd,
+    onRemove,
+    products,
   }
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
